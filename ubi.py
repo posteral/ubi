e232c37515ee9d4c5c56f6f158beb0696244e700
Filipe Posteral <filipe.posteral@contentsquare.com>
2018-01-22 15:32:37 +0100

    Use external configuration file.

--- ubi.py
+++ ubi.py
@@ -1,4 +1,9 @@
 import requests
+import dbconfig as cfg
 
 r = requests.post("http://bugs.python.org", data={'number': 12524, 'type': 'issue', 'action': 'show'})
 print(r.status_code, r.reason)
+print(cfg.uxpc_alerts_db_staging_config)
+
+
+#first we need to find a way to have a configuration file
