03d4adb25ba808f32a7e08ca37cf7089914bfde5
Filipe Posteral <filipe.posteral@contentsquare.com>
2018-01-22 16:25:18 +0100

    Save ES parameters in local database.

--- ubi.py
+++ ubi.py
@@ -8,12 +8,24 @@ import serviceConfig as cfg
 uri = str(cfg.staging_config['host'])+':'+str(cfg.staging_config['port'])+str(cfg.routes['fetch-alert-configs'])
 print(uri)
 
-#query service
+#fetch alerts
 r = requests.post(uri)
 
 #parse response
 json_data = json.loads(r.text)
 
 alert_es_parameters_array = json_data['alertESParametersArray']
-
 print(alert_es_parameters_array)
+
+#save in local db
+
+uri = str(cfg.local_config['host'])+':'+str(cfg.local_config['port'])+str(cfg.routes['save-alert-esparameters'])
+print(uri)
+
+#save alert
+alert = data = alert_es_parameters_array[0]
+print(alert)
+
+r = requests.post(uri, json = alert)
+
+print(r)
