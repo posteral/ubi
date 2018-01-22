f06fa6223a24c6974f0d7f78f2a99f3075211a10
Filipe Posteral <filipe.posteral@contentsquare.com>
2018-01-22 16:17:23 +0100

    Parse response.

--- ubi.py
+++ ubi.py
@@ -1,10 +1,19 @@
+import json
+
 import requests
+
 import serviceConfig as cfg
 
-
+#build URI from configuration file
 uri = str(cfg.staging_config['host'])+':'+str(cfg.staging_config['port'])+str(cfg.routes['fetch-alert-configs'])
 print(uri)
-r = requests.post(uri, data={'number': 12524, 'type': 'issue', 'action': 'show'})
-print(r.status_code, r.reason)
-print(cfg.staging_config)
-print(r.json())
+
+#query service
+r = requests.post(uri)
+
+#parse response
+json_data = json.loads(r.text)
+
+alert_es_parameters_array = json_data['alertESParametersArray']
+
+print(alert_es_parameters_array)
