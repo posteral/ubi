87c0c97537403ba3c22e7d4ddc2dcd18949e0b6b
Filipe Posteral <filipe.posteral@contentsquare.com>
2018-01-22 16:54:39 +0100

    Check which metric values are stored in database.

--- ubi.py
+++ ubi.py
@@ -31,7 +31,21 @@ for alert_es_parameters in alert_es_parameters_array:
 uri = str(cfg.local_config['host'])+':'+str(cfg.local_config['port'])+str(cfg.routes['get-metric-values'])
 print(uri)
 
+response_array = []
 for alert_configuration in alert_configuration_array:
     print(alert_configuration)
     r = requests.post(uri, json=alert_configuration)
+    response_array.append(json.loads(r.text))
     print(r.json())
+
+
+alert_confs_and_responses = zip(alert_configuration_array, response_array)
+
+#check which values aren't in database and calculate them
+
+for alert_conf_and_response in alert_confs_and_responses:
+    alert_conf = alert_conf_and_response[0]
+    response = alert_conf_and_response[1]
+    time_window_0_value = response['timeWindow0Value']
+    time_window_1_value = response['timeWindow1Value']
+    print("oi")
