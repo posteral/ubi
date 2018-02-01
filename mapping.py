beead0489e56746b4d826aa0bf7d2082a365cfa2
Filipe Posteral <filipe.posteral@contentsquare.com>
2018-02-01 17:56:53 +0100

    Get projects and mappings per project.

--- mapping.py
+++ mapping.py
@@ -4,7 +4,7 @@ import requests
 
 import utils
 
-def how_many_projects_in_env(env):
+def get_projects_in_env(env):
     projects = []
     uri = env.pp_base_uri() + '/projects'
     response = requests.get(uri)
@@ -15,23 +15,19 @@ def how_many_projects_in_env(env):
         projects.append((project['id'], project['name']))
     return projects
 
-def how_many_mappings_in_project(project_name, project_id):
-    return 10
+def get_mappings_in_project(env, p_id, p_name):
+    uri = env.pp_base_uri() + '/projects/' + str(p_id) + '/mappings'
+    response = requests.get(uri)
+    data = json.loads(response.text)
+    print('# of mappings in project '+str(p_id)+' ('+p_name+'): '+str(len(data)))
+    #return something
 
-#Get all mappings belonging to a project
-#More info on the endpoint: http://doc.csq.io/project-parameters/#api-Mapping-getAll
+TEST_ENV = utils.Env.PRODUCTION
 
-TEST_ENV = utils.Env.NEXT2
+projects = get_projects_in_env(TEST_ENV)
 
-projects = how_many_projects_in_env(TEST_ENV)
-
-project_id = 269
-pp_uri = TEST_ENV.pp_base_uri() + '/projects/' + str(project_id) + '/mappings'
-print(pp_uri)
-r = requests.get(pp_uri)
-json_data = json.loads(r.text)
-
-
-print('lala')
-
+for project in projects:
+    project_id = project[0]
+    project_name = project[1]
+    get_mappings_in_project(TEST_ENV, project_id, project_name)
 
