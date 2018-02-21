8454e32672968050b85cf33acf6d7c4aa0242b30
Filipe Posteral <filipe.posteral@contentsquare.com>
2018-02-21 17:27:31 +0100

    Analyse pageviews.

--- pv_analysis.py
+++ pv_analysis.py
@@ -4,7 +4,7 @@ data = file_lines[0].replace(')','').split(", (") #we just have one line
 
 for project_pageviews in data:
     split = project_pageviews.replace('(','').split(',')
-    project_id = int(split[0])
+    project_id =  int(split[0])
     project_name = split[1]
     pageviews = int(split[2])
     print(str(project_id)+';'+project_name+';'+str(pageviews))
